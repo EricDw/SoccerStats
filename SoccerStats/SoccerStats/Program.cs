ddd3c7ebd95da16d9a61ac4e6abc7a4f346a2cda
Eric De Wildt <eric.dewildt@gmail.com>
2017-03-04 00:01:18 -0500

    Added method adding lines from a file to a list and seperating them by commas.

--- SoccerStats/SoccerStats/Program.cs
+++ SoccerStats/SoccerStats/Program.cs
@@ -13,17 +13,37 @@ namespace SoccerStats
         {
             string currentDircetory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDircetory);
-            var fileName = Path.Combine(directory.FullName, "data.txt");
-            var file = new FileInfo(fileName);
-
-            if(file.Exists)
+            var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
+            var fileContents = ReadFile(fileName);
+            string[] fileLines = fileContents.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in fileLines)
             {
-                using (var reader = new StreamReader(file.FullName))
-                { 
-                    Console.SetIn(reader);
-                    Console.WriteLine(Console.ReadLine());
+                Console.WriteLine(line);
+            }
+        }
+
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            } 
+        }
+
+        public static List<string[]> ReadSoccerResults(string fileName)
+        {
+            var soccerResults = new List<string[]>();
+            using (var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                while((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+                    soccerResults.Add(values);
                 }
             }
+
+            return soccerResults;
         }
     }
 }
