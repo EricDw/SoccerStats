360a49621ad66274d361c7e3cff71e6103c68405
Eric De Wildt <eric.dewildt@gmail.com>
2017-03-11 18:26:31 -0500

    Added Player class from JSON file and then cleaned it up.

---
+++ SoccerStats/SoccerStats/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoccerStats
+{
+
+    public class Rootobject
+    {
+        public Player[] Players { get; set; }
+    }
+
+    public class Player
+    {
+        public string first_name { get; set; }
+        public int id { get; set; }
+        public string points_per_game { get; set; }
+        public string second_name { get; set; }
+        public string team_name { get; set; }
+    }
+
+}
